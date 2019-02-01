9b12ab0bcd26b117014f5a4afd2f443900741299
Dennis Gladun <dgl@covis.de>
2019-02-01 16:05:14 +0100

    fix

--- massage/src/app/app-routing.module.ts
+++ massage/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { LoginComponent } from './pages/login/login.component';
+import { HomeLayoutComponent } from './layouts/home-layout.component';
+import { AuthGuard } from './services/auth-guard.service';
 
 const routes: Routes = [
+
+  //Site routes goes here
   {
     path: '', component: HomeLayoutComponent, canActivate: [AuthGuard], runGuardsAndResolvers: 'always', children: [
       {path: 'text', loadChildren: './pages/text/text.module#TextModule' }
@@ -11,7 +16,8 @@ const routes: Routes = [
   //no layout routes
   { path: 'login', component: LoginComponent },
   // otherwise redirect to home
-  {path: '**', redirectTo: '/'}
+  { path: '**', redirectTo: '/' }
+
 ];
 
 @NgModule({
